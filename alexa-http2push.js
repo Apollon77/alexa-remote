7d57121826305da3df2604b6b66bb722a886d128
Daniel <49846893+danielbrunt57@users.noreply.github.com>
2024-10-19 04:35:47 -0700

    Add additional Amazon domains for host connection lookup
    'amazon.ca', 'amazon.com.mx':
    host: 'bob-dispatch-prod-na.amazon.com'
    
    'amazon.com.in', 'amazon.co.nz':
    host: 'bob-dispatch-prod-fe.amazon.com'

--- alexa-http2push.js
+++ alexa-http2push.js
@@ -35,11 +35,19 @@ class AlexaHttp2Push extends EventEmitter {
                 host = this._options.pushDispatchHost;
             } else if (this._options.amazonPage === 'amazon.com') {
                 host = 'bob-dispatch-prod-na.amazon.com';
+            } else if (this._options.amazonPage === 'amazon.ca') {
+                host = 'bob-dispatch-prod-na.amazon.com';
+            } else if (this._options.amazonPage === 'amazon.com.mx') {
+                host = 'bob-dispatch-prod-na.amazon.com';
             } else if (this._options.amazonPage === 'amazon.com.br') {
                 host = 'bob-dispatch-prod-na.amazon.com';
             } else if (this._options.amazonPage === 'amazon.co.jp') {
                 host = 'bob-dispatch-prod-fe.amazon.com';
             } else if (this._options.amazonPage === 'amazon.com.au') {
+                host = 'bob-dispatch-prod-fe.amazon.com';
+            } else if (this._options.amazonPage === 'amazon.com.in') {
+                host = 'bob-dispatch-prod-fe.amazon.com';
+            } else if (this._options.amazonPage === 'amazon.co.nz') {
                 host = 'bob-dispatch-prod-fe.amazon.com';
             }
             this._options.logger && this._options.logger(`Alexa-Remote HTTP2-PUSH: Use host ${host}`);
